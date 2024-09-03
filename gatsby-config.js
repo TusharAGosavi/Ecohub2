4add0a0c4283b467680dd0c842d54d95ce6635a2
Tushar Gosavi <tushargosavi09@gmail.com>
2024-09-04 01:06:53 +0530

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,10 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby + Netlify CMS Starter",
-    description:
-      "This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.",
-  },
+    title: "EcoHub CMS",
+    description: "A content management system built to promote sustainability.",
+    author: "Tushar",
+}
+  ,
   plugins: [
     "gatsby-plugin-react-helmet",
     {
