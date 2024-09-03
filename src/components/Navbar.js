42eec9b0b72838fe593d320f0bf2a83157f62f7a
Tushar Gosavi <tushargosavi09@gmail.com>
2024-09-04 01:09:28 +0530

    first commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -66,7 +66,7 @@ const Navbar = () => {
           <li className="navbar-end has-text-centered">
             <a
               className="navbar-item"
-              href="https://github.com/decaporg/gatsby-plugin-decap-cms"
+              href="https://github.com/TusharAGosavi/Ecohub2.git"
               target="_blank"
               rel="noopener noreferrer"
             >
